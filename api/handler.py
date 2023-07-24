a24fdf28fdfde1f8e6853a3da465672a241efab6
ezequielpf <ezequielpf@gmail.com>
2023-07-24 11:08:18 -0300

    no changes

--- api/handler.py
+++ api/handler.py
@@ -27,23 +27,5 @@ def rossmann_predict():
     df_response = pipeline.get_prediction(model, test_raw, df3)
     return df_response
 
-    # if test_json: # there is data
-    #     if isinstance(test_json, dict): # unique example
-    #         test_raw = pd.DataFrame(test_json, index=[0])
-    #     else:   # multiple example
-    #         test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
-    #     pipeline = Rossmann()
-    #     # data cleaning
-    #     df1 = pipeline.data_cleaning(test_raw)
-    #     # feature engineering
-    #     df2 = pipeline.feature_engineering(df1)
-    #     # data preparation
-    #     df3 = pipeline.data_preparation(df2)
-    #     # prediction
-    #     df_response = pipeline.get_prediction(model, test_raw, df3)
-    #     return df_response
-    # else:
-    #     return Response('{}', status=200, mimetype='application/json')
-
 if __name__ == '__main__':
     app.run(port=5000, host='localhost', debug=False)
